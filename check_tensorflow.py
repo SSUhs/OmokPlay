46b970bbaa9840a94c79248f172681619e5e77e5
임현성 <wlcjsk32@gmail.com>
2022-10-30 00:24:12 +0900

    10301224

--- check_tensorflow.py
+++ check_tensorflow.py
@@ -8,12 +8,11 @@ def check_tf(compile_env):
         print(devices)
     elif compile_env == 'colab':
         import tensorflow as tf
+        print("\n")
         print(tf.__version__)
         devices = tf.config.list_physical_devices()
         print(devices)
-        if devices[0].device_type == 'CPU':  # CPU 환경에서는 학습 X
-            print("Colab에서 실행시 GPU 가속 환경으로 변경해주세요")
-            quit()
+        print(f'디바이스 : {devices[0]}')
     else:
         print("잘못된 파라미터")
         quit()
