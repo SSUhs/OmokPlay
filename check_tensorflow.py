d93ca6926c24b125eb89b2fa338e0f1a8b784f2e
임현성 <wlcjsk32@gmail.com>
2022-11-01 01:26:53 +0900

    11010126

--- check_tensorflow.py
+++ check_tensorflow.py
@@ -15,10 +15,15 @@ def check_tf(compile_env):
         device_name = tf.test.gpu_device_name()
 
         # GPU를 사용하거나 TPU를 사용하지 않으면 종료 (혹시나 가속기를 안켜놓았을 상황을 방지)
-        if device_name != '/device:GPU:0': # GPU 환경인 경우
-            raise SystemError('오류 : GPU를 사용하고 있지 않습니다')
-            quit()
-        print('Found GPU at: {}'.format(device_name))
+        if device_name != '/device:GPU:0': # GPU 안쓰는 환경인 경우
+            mode = int(input("현재 GPU 가속이 미사용 상태입니다. CPU 환경으로만 진행하려면 0, 종료할려면 1"))
+            if mode != 0:
+                print("종료 합니다")
+                quit()
+        else:
+            print('Found GPU at: {}'.format(device_name))
+        # TPU 사용하게 되면 TPU도 예외 처리
+        # https://colab.research.google.com/notebooks/tpu.ipynb#scrollTo=FpvUOuC3j27n
         # else:
         #     try:
         #         tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
