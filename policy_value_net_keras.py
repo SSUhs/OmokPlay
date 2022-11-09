34505ff3883827f903b0d110d1678f47f6f1b83c
임현성 <wlcjsk32@gmail.com>
2022-11-10 00:34:07 +0900

    11100034

--- policy_value_net_keras.py
+++ policy_value_net_keras.py
@@ -22,6 +22,20 @@ import numpy as np
 import pickle
 
 
+def cpu():
+    with tf.device('/cpu:0'):
+        random_image_cpu = tf.random.normal((100, 100, 100, 3))
+        net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
+        return tf.math.reduce_sum(net_cpu)
+
+
+def gpu():
+    with tf.device('/device:GPU:0'):
+        random_image_gpu = tf.random.normal((100, 100, 100, 3))
+        net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
+        return tf.math.reduce_sum(net_gpu)
+
+
 class PolicyValueNetKeras():
     """policy-value network """
 
@@ -179,14 +193,4 @@ class PolicyValueNetKeras():
         net_params = self.get_policy_param()
         pickle.dump(net_params, open(model_file, 'wb'), protocol=2)
 
-def cpu():
-    with tf.device('/cpu:0'):
-        random_image_cpu = tf.random.normal((100, 100, 100, 3))
-        net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
-        return tf.math.reduce_sum(net_cpu)
 
-def gpu():
-    with tf.device('/device:GPU:0'):
-        random_image_gpu = tf.random.normal((100, 100, 100, 3))
-        net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
-        return tf.math.reduce_sum(net_gpu)
