d86bc5a84dac385dc2c937d01148bc8792e97929
임현성 <wlcjsk32@gmail.com>
2022-11-10 01:49:41 +0900

    11100149

--- policy_value_net_keras.py
+++ policy_value_net_keras.py
@@ -82,26 +82,21 @@ class PolicyValueNetKeras():
         devices_all = tf.config.list_physical_devices()
         print(f'활성 Device : {devices_all}\n')
         device_name = tf.test.gpu_device_name()
-        if not tf.test.is_gpu_available():
-            print("GPU를 가속하지 않으면 사용할 수 없습니다")
-            quit()
         # GPU를 사용하거나 TPU를 사용하지 않으면 종료 (혹시나 가속기를 안켜놓았을 상황을 방지)
         if device_name != '/device:GPU:0':  # GPU 안쓰는 환경인 경우
-            print("GPU를 가속하지 않으면 사용할 수 없습니다")
-            quit()
-            # mode = int(input("현재 GPU 가속이 미사용 상태입니다. CPU 환경으로만 진행하려면 0, 종료할려면 1"))
-            # if mode != 0:
-            #     print("종료 합니다")
-            #     quit()
+            mode = int(input("현재 GPU 가속이 미사용 상태입니다. CPU 환경으로만 진행하려면 0, 종료할려면 1"))
+            if mode != 0:
+                print("종료 합니다")
+                quit()
         else:
             print('Found GPU at: {}'.format(device_name))
+            test_keras_environment()
         self.board_width = board_width
         self.board_height = board_height
         self.l2_const = 1e-4  # coef of l2 penalty
         self.create_policy_value_net()
         self._loss_train_op()
 
-        test_keras_environment()
         if (model_file is not None) and (keras_init_num != 0):
             net_params = pickle.load(open(model_file, 'rb'))
             self.model.set_weights(net_params)
