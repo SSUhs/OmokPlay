7d238f75ede103f6ccdc92d83d146457307027c3
임현성 <wlcjsk32@gmail.com>
2022-11-09 23:29:38 +0900

    11091129

--- train_local.py
+++ train_local.py
@@ -12,43 +12,46 @@ from time import gmtime
 
 from save_data_helper import save_data_helper
 
-sys.setrecursionlimit(10**8)
-
+sys.setrecursionlimit(10 ** 8)
 
 list_train_num = []
 list_loss = []
 list_time = []
-list_batch_size = [] # 22.11.08 오전 1시 새로 추가
-def add_csv_data(train_num,loss,batch_size):
+list_batch_size = []  # 22.11.08 오전 1시 새로 추가
+
+
+def add_csv_data(train_num, loss, batch_size):
     list_train_num.append(train_num)
     list_time.append(datetime.now())
     list_loss.append(loss)
     list_batch_size.append(batch_size)
 
-def make_csv_file(board_size,last_train_num):
-    df = DataFrame({'train_num':Series(list_train_num),'time':Series(list_time),'loss':Series(list_loss), 'batch_size':Series(list_batch_size)})
+
+def make_csv_file(board_size, last_train_num):
+    df = DataFrame({'train_num': Series(list_train_num), 'time': Series(list_time), 'loss': Series(list_loss),
+                    'batch_size': Series(list_batch_size)})
     df.to_csv(f'/content/drive/MyDrive/{board_size}x{board_size}_{last_train_num}.csv', header=False, index=False)
-    list_train_num.clear() # 초기화
+    list_train_num.clear()  # 초기화
     list_time.clear()  # 초기화
     list_loss.clear()  # 초기화
     list_batch_size.clear()
 
 
 class TrainPipeline():
-    def __init__(self, board_width, board_height, train_environment,ai_lib,tf_model_file=None,keras_model_file=None,tf_init_num=0,tf_lr_data=None,keras_lr_data=None,keras_init_num=0): # tf_model_file : 텐서플로우 모델 파일
+    def __init__(self, board_width, board_height, train_environment, ai_lib, tf_model_file=None, keras_model_file=None,
+                 tf_init_num=0, tf_lr_data=None, keras_lr_data=None, keras_init_num=0):  # tf_model_file : 텐서플로우 모델 파일
         # 훈련 환경 : train_environment = 1 >> 코랩 / = 2 >> 로컬에서 학습
         self.train_environment = train_environment
         self.tf_lr_data = tf_lr_data
 
         # 학습 라이브러리
-        self.ai_lib = ai_lib # tensorflow 또는 theano
+        self.ai_lib = ai_lib  # tensorflow 또는 theano
 
         # 게임(오목)에 대한 변수들
         self.board_width, self.board_height = board_width, board_height
         self.n_in_row = 5
         self.board = Board(width=self.board_width, height=self.board_height, n_in_row=self.n_in_row)
-        self.game = Game(self.board,is_gui_mode = False)
-
+        self.game = Game(self.board, is_gui_mode=False)
 
         # 학습에 대한 변수들
         self.learn_rate = 2e-3
@@ -86,9 +89,13 @@ class TrainPipeline():
             from policy_value_net_tensorflow import PolicyValueNetTensorflow
             self.make_tensorflow_checkpoint_auto(tf_init_num)
             if ai_lib == 'tensorflow':
-                self.policy_value_net = PolicyValueNetTensorflow(self.board_width, self.board_height,model_file=tf_model_file, compile_env='colab',init_num=tf_init_num)
+                self.policy_value_net = PolicyValueNetTensorflow(self.board_width, self.board_height,
+                                                                 model_file=tf_model_file, compile_env='colab',
+                                                                 init_num=tf_init_num)
             elif ai_lib == 'tensorflow-1.15gpu':  # tensorflow-1.15gpu
-                self.policy_value_net = PolicyValueNetTensorflow(self.board_width, self.board_height,model_file=tf_model_file,compile_env='colab-1.15gpu',init_num=tf_init_num)
+                self.policy_value_net = PolicyValueNetTensorflow(self.board_width, self.board_height,
+                                                                 model_file=tf_model_file, compile_env='colab-1.15gpu',
+                                                                 init_num=tf_init_num)
             if not self.tf_lr_data is None:
                 try:
                     with open(tf_lr_data, 'rb') as file:
@@ -98,26 +105,29 @@ class TrainPipeline():
                         self.data_buffer = saved_data.data_buffer
                         print("\nTrainPipeLine 데이터를 로딩했습니다")
                         print(f'learning_rate : {self.learn_rate} lr_multiplier : {self.lr_multiplier}')
-                except: print("\ntrain_num이 0이 아닌 상황에서 learning_rate 데이터가 존재하지 않거나 로딩에 실패하였습니다")
+                except:
+                    print("\ntrain_num이 0이 아닌 상황에서 learning_rate 데이터가 존재하지 않거나 로딩에 실패하였습니다")
         elif ai_lib == 'keras':
             from policy_value_net_keras import PolicyValueNetKeras
-            self.policy_value_net = PolicyValueNetKeras(self.board_width,self.board_height,compile_env='colab',model_file=keras_model_file,init_num=keras_init_num,keras_lr_data=keras_lr_data)
+            self.policy_value_net = PolicyValueNetKeras(self.board_width, self.board_height, compile_env='colab',
+                                                        model_file=keras_model_file, keras_init_num=init_num,
+                                                        keras_lr_data=keras_lr_data)
         else:
             print("존재하지 않는 라이브러리입니다")
             quit()
 
         # 훈련할 떄 사용할 플레이어 생성
-        self.mcts_player = MCTSPlayer(self.policy_value_net.policy_value_fn, c_puct=self.c_puct, n_playout=self.n_playout, is_selfplay=1)
+        self.mcts_player = MCTSPlayer(self.policy_value_net.policy_value_fn, c_puct=self.c_puct,
+                                      n_playout=self.n_playout, is_selfplay=1)
 
-
-    def make_tensorflow_checkpoint_auto(self,init_num):  # 구글 드라이브 체크포인트 자동 생성
-        if init_num ==0:
+    def make_tensorflow_checkpoint_auto(self, init_num):  # 구글 드라이브 체크포인트 자동 생성
+        if init_num == 0:
             return
         save_path = '/content/drive/MyDrive/checkpoint'
         model_name = f'tf_policy_{self.board_width}_{init_num}_model'
         # str = f'\"model_checkpoint_path: \"/content/drive/MyDrive/{model_name}\"\nall_model_checkpoint_paths: "/content/drive/MyDrive/{model_name}\"\n'
         str2 = f'\"model_checkpoint_path: \"/content/drive/MyDrive/{model_name}\"\n'
-        with open(save_path,"w") as f:
+        with open(save_path, "w") as f:
             f.write(str2)
             print("체크 포인트 자동 생성")
 
@@ -146,7 +156,7 @@ class TrainPipeline():
             play_data = list(play_data)[:]
             self.episode_len = len(play_data)
             play_data = self.get_equi_data(play_data)  # 데이터를 뒤집어서 경우의 수를 더 확대
-            self.data_buffer.extend(play_data) # deque의 오른쪽(마지막)에 삽입
+            self.data_buffer.extend(play_data)  # deque의 오른쪽(마지막)에 삽입
 
     # 자가 훈련을 통해 정책 업데이트 하는 부분
     # 플레이어와 대결 할 때는 이 함수가 호출 되지 않는다 >> 따라서 플레이어와 AI가 대결할 때는 정책 업데이트 X
@@ -160,23 +170,25 @@ class TrainPipeline():
         old_probs, old_v = self.policy_value_net.policy_value(state_batch)
         for i in range(self.epochs):
             # loss, entropy = self.policy_value_net.train_step(state_batch, mcts_probs_batch, winner_batch, self.learn_rate*self.lr_multiplier)
-            loss = self.policy_value_net.train_step(state_batch, mcts_probs_batch, winner_batch, self.learn_rate*self.lr_multiplier)
+            loss = self.policy_value_net.train_step(state_batch, mcts_probs_batch, winner_batch,
+                                                    self.learn_rate * self.lr_multiplier)
             new_probs, new_v = self.policy_value_net.policy_value(state_batch)
             kl = np.mean(np.sum(old_probs * (np.log(old_probs + 1e-10) - np.log(new_probs + 1e-10)), axis=1))
 
             # D_KL diverges 가 나쁘면 빠른 중지
-            if kl > self.kl_targ * 4 : break
+            if kl > self.kl_targ * 4: break
 
         # learning rate를 적응적으로 조절
-        if kl > self.kl_targ * 2 and self.lr_multiplier > 0.1 : self.lr_multiplier /= 1.5
-        elif kl < self.kl_targ / 2 and self.lr_multiplier < 10 : self.lr_multiplier *= 1.5
+        if kl > self.kl_targ * 2 and self.lr_multiplier > 0.1:
+            self.lr_multiplier /= 1.5
+        elif kl < self.kl_targ / 2 and self.lr_multiplier < 10:
+            self.lr_multiplier *= 1.5
 
         explained_var_old = (1 - np.var(np.array(winner_batch) - old_v.flatten()) / np.var(np.array(winner_batch)))
         explained_var_new = (1 - np.var(np.array(winner_batch) - new_v.flatten()) / np.var(np.array(winner_batch)))
 
-
-        print(f"kl:{kl:5f}, lr_multiplier:{self.lr_multiplier:3f}, loss:{loss}, data_buffer size() : {len(self.data_buffer)}, explained_var_old:{explained_var_old:3f}, explained_var_new:{explained_var_new:3f}")
-
+        print(
+            f"kl:{kl:5f}, lr_multiplier:{self.lr_multiplier:3f}, loss:{loss}, data_buffer size() : {len(self.data_buffer)}, explained_var_old:{explained_var_old:3f}, explained_var_new:{explained_var_new:3f}")
 
         return loss
 
@@ -187,42 +199,47 @@ class TrainPipeline():
         for i in range(self.game_batch_num):
             self.collect_selfplay_data(self.play_batch_size)
             self.train_num += 1
-            before_time_gap = time()-before_time
+            before_time_gap = time() - before_time
             print(f"\n소요시간 : {before_time_gap}초, 게임 플레이 횟수 :{self.train_num}, episode_len:{self.episode_len}")
             before_time = time()
 
             if len(self.data_buffer) > self.batch_size:
                 loss = self.policy_update()
-                add_csv_data(train_num=self.train_num,loss=loss,batch_size=self.batch_size)
+                add_csv_data(train_num=self.train_num, loss=loss, batch_size=self.batch_size)
 
             # 현재 model의 성능을 체크, 모델 속성을 저장
             # check_freq 횟수 마다 저장 (check_freq가 50이면 50번 훈련마다 한번씩 저장)
-            if (i+1) % self.check_freq == 0:
+            if (i + 1) % self.check_freq == 0:
                 print(f"★ {self.train_num}번째 batch에서 모델 저장 : {datetime.now()}")
                 # .model 파일은 플레이할 때 사용할 모델 파일이고, pickle 파일은 학습 데이터? (실제 게임에서는 .model, 학습 과정에서는 .pickle을 불러 와야한다)
-                if self.train_environment == 1: # 코랩 (구글 드라이브 연동)
+                if self.train_environment == 1:  # 코랩 (구글 드라이브 연동)
                     if self.ai_lib == 'theano':
-                        self.policy_value_net.save_model(f'/content/drive/MyDrive/policy_{self.board_width}_{self.train_num}.model')
-                        pickle.dump(self, open(f'/content/drive/MyDrive/train_{self.board_width}_{self.train_num}.pickle', 'wb'), protocol=2) # theano만 pickle로 저장
-                        make_csv_file(self.board_width,self.train_num)
+                        self.policy_value_net.save_model(
+                            f'/content/drive/MyDrive/policy_{self.board_width}_{self.train_num}.model')
+                        pickle.dump(self,
+                                    open(f'/content/drive/MyDrive/train_{self.board_width}_{self.train_num}.pickle',
+                                         'wb'), protocol=2)  # theano만 pickle로 저장
+                        make_csv_file(self.board_width, self.train_num)
                     elif self.ai_lib == 'tensorflow' or self.ai_lib == 'tensorflow-1.15gpu':
-                        self.policy_value_net.save_model(f'/content/drive/MyDrive/tf_policy_{self.board_width}_{self.train_num}_model')
-                        make_csv_file(self.board_width,self.train_num)
-                        data_helper = save_data_helper(self.train_num,self.board_width,self.learn_rate,self.lr_multiplier,self.data_buffer)
+                        self.policy_value_net.save_model(
+                            f'/content/drive/MyDrive/tf_policy_{self.board_width}_{self.train_num}_model')
+                        make_csv_file(self.board_width, self.train_num)
+                        data_helper = save_data_helper(self.train_num, self.board_width, self.learn_rate,
+                                                       self.lr_multiplier, self.data_buffer)
                         data_helper.save_model_data()  # lr_multiplier 저장
                     else:
                         print("사용할 수 없는 라이브러리입니다")
                         quit()
                     print("학습 파일을 저장하였습니다")
-                elif self.train_environment == 2: # 로컬
-                    self.policy_value_net.save_model(f'{model_path_theano}/policy_{self.board_width}_{self.train_num}.model')
-                    pickle.dump(self, open(f'{train_path_theano}/train_{self.board_width}_{self.train_num}.pickle', 'wb'), protocol=2)
+                elif self.train_environment == 2:  # 로컬
+                    self.policy_value_net.save_model(
+                        f'{model_path_theano}/policy_{self.board_width}_{self.train_num}.model')
+                    pickle.dump(self,
+                                open(f'{train_path_theano}/train_{self.board_width}_{self.train_num}.pickle', 'wb'),
+                                protocol=2)
                 else:
                     print("존재하지 않는 환경입니다")
                     quit()
-
-
-
 
 
 if __name__ == '__main__':
@@ -236,13 +253,11 @@ if __name__ == '__main__':
     train_path_theano = f"./save/train_{size}"
     model_path_theano = f"./save/model_{size}"
 
-
     print("실행 환경을 입력해주세요\n1: Colab\n2: Local\n")
     train_environment = int(input())
     if not (train_environment == 1 or train_environment == 2):
         print("존재하지 않는 환경입니다")
         quit()
-
 
     print("학습에 이용할 라이브러리를 선택해주세요 : \'keras\' 또는 \'tensorflow\' 또는 \'theano\'\n")
     ai_lib = input()
@@ -254,7 +269,7 @@ if __name__ == '__main__':
     init_num = int(input())
 
     if ai_lib == 'theano':
-        if train_environment == 1: # colab + google drive
+        if train_environment == 1:  # colab + google drive
             if init_num == 0 or init_num == None:
                 training_pipeline = TrainPipeline(size, size, train_environment, ai_lib)
             else:
@@ -268,13 +283,14 @@ if __name__ == '__main__':
         if init_num == 0 or init_num == None:
             tf_model_file = None
             tf_lr_data = None
-        elif train_environment == 1: # colab + google drive
+        elif train_environment == 1:  # colab + google drive
             tf_model_file = f'/content/drive/MyDrive/tf_policy_{size}_{init_num}_model'
             tf_lr_data = f'/content/drive/MyDrive/tf_train_{size}_{init_num}.pickle'
-        else: # 로컬
+        else:  # 로컬
             tf_model_file = f'./model/tf_policy_{size}_{init_num}_model'
             tf_lr_data = f'./model/tf_train_{size}_{init_num}.pickle'
-        training_pipeline = TrainPipeline(size, size, train_environment, ai_lib,tf_model_file=tf_model_file,tf_init_num=init_num,tf_lr_data=tf_lr_data)
+        training_pipeline = TrainPipeline(size, size, train_environment, ai_lib, tf_model_file=tf_model_file,
+                                          tf_init_num=init_num, tf_lr_data=tf_lr_data)
     elif ai_lib == 'keras':
         if init_num == 0 or init_num == None:
             keras_model_file = None
@@ -286,7 +302,7 @@ if __name__ == '__main__':
             print("학습이 불가능한 환경입니다")
             quit()
         training_pipeline = TrainPipeline(size, size, train_environment, ai_lib, keras_model_file=keras_model_file,
-                                              keras_init_num=init_num, keras_lr_data=keras_lr_data)
+                                          keras_init_num=init_num, keras_lr_data=keras_lr_data)
     else:
         print("없는 경우")
         quit()
@@ -316,5 +332,3 @@ if __name__ == '__main__':
     #             quit()
     #     else:
     #         print("존재하지 않는 tra
-
-
