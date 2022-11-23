6196df4c62bdc69e11aae6a4656e24162cf1f3f8
임현성 <wlcjsk32@gmail.com>
2022-11-23 15:24:50 +0900

    11231524

--- test2.py
+++ test2.py
@@ -1,17 +1,59 @@
-import copy
 import numpy as np
 
-class t:
-    def get_best_idx(self, board):
-        probs = self.model.predict(input)
-        probs_tmp = copy.deepcopy(probs)
-        while True:
-            best_index = np.argmax(probs_tmp[0])
-            # 이미 돌이 있는 자리를 선택하거나 금수에 놓은 경우
-            if self.is_banned_pos(board, best_index):
-                probs_tmp[0][best_index] = -1  # 금수 자리는 선택 불가능 하게 설정
-                continue
-            else:
-                break
-        return best_index
+
+# ex) 0~224 형태의 보드판을 15*15 형태로 변경
+def convert_1nd_board_to_2nd(arr_1nd,board_size):
+    return np.reshape(arr_1nd,(board_size,board_size))
+
+
+def convert_2nd_board_to_1nd(arr_2nd):
+    return np.ravel(arr_2nd)
+
+# 0도(원본),90도,180도,270도 회전 + 각각마다 상하,좌우 반전 >> 하나의 상태로 12가지 상태 추출
+def get_rotate_board_12dir(arr):
+    all_list = []
+    for i in range(4):
+        rotates = np.rot90(arr,i)
+        flip_horizontal = np.fliplr(rotates)
+        flip_vertical = np.flipud(rotates)
+        all_list.append(rotates)
+        all_list.append(flip_horizontal)
+        all_list.append(flip_vertical)
+    return all_list
+
+def convert_to_label_start0(x, y,board_size):
+    return board_size * y + x
+
+def get_rotate_label(label,board_size):
+    arr = convert_label_to_board(label,board_size=board_size)
+    rotate_list = get_rotate_board_12dir(arr) # len = 12
+    all_list = []
+    for i in range(len(rotate_list)):
+        label_arr = rotate_list[i]
+        y,x = np.where(label_arr == 1)
+        move = convert_to_label_start0(x[0],y[0],board_size) # ndarray 형태로 나오기 때문에 0번으로 접근
+        all_list.append(move)
+    return all_list
+
+
+# move : 15*15의 경우 0~224
+# 레이블을 board형태로 전환
+def convert_label_to_board(move,board_size):
+    y = move//board_size
+    x = move-(y*board_size)
+    tmp = np.zeros([board_size,board_size])
+    tmp[y][x] = 1
+    return tmp
+
+
+
+
+test_1nd = np.linspace(0, 224, 225)
+arr_2nd = convert_1nd_board_to_2nd(test_1nd,15)
+arr_1nd = convert_2nd_board_to_1nd(arr_2nd)
+arr = np.zeros([5,5])
+arr[1][2] = 1
+arr[2][3] = 2
+board_size = 5
+print(get_rotate_label(13,board_size))
 
--- train_dataset.py
+++ train_dataset.py
@@ -3,14 +3,11 @@ import copy
 import numpy as np
 import codecs
 import csv
-from copy import deepcopy
 import tensorflow as tf
 from tensorflow.keras.layers import Activation, Dense, Conv2D, Flatten
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
-
-from random import randint
 from tensorflow.keras.models import load_model
 import keras.backend as K
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
@@ -24,6 +21,7 @@ path_saved_weights = '/content/drive/MyDrive/saved_data/weights/'
 
 
 def convert_load_dataset(csv_file_name, is_one_hot_encoding,type_train):
+    board_size = 15
     data_x_p_black = []  # 흑 정책망 input
     data_x_p_white = []  # 백 정책망 input
     labels_p_black = []  # 흑 정책망 레이블
@@ -38,6 +36,7 @@ def convert_load_dataset(csv_file_name, is_one_hot_encoding,type_train):
         print(f"존재 하지 않는 type_train : {type_train}")
         quit()
 
+
     print("\n데이터 셋 로딩 시작..")
     with open(csv_file_name, 'r') as f:
         next(f, None)
@@ -47,26 +46,44 @@ def convert_load_dataset(csv_file_name, is_one_hot_encoding,type_train):
         # 헤더 : move 위치 / black_value / 누가 돌을 놓을차례(흑1, 백2) / 상태~
         for row in reader: # row는 문자열 리스트
             count_read += 1
-            if float(row[1]) <= -100: # 승부 판별 불가능
-                skip_count+=1
-                continue
-            if type_train == 0: # 흑 정책망 학습
-                # 일단 전부 다 학습
+            # if float(row[1]) <= -100: # 승부 판별 불가능
+            #     skip_count+=1
+            #     continue
+            if type_train == 0: # 흑,백 정책망 학습
                 # if float(row[1]) <= 0.5 and int(float(row[2]) == 1):  # 흑이 이기거나 비기는 경우면서 흑이 돌을 놓을 차례인 경우
-                labels_p_black.append(int(float(row[0])))
-                data_x_p_black.append(row[3:])
+                label = int(float(row[0])) # 정답 라벨
+                board_2nd = convert_1nd_board_to_2nd(np.array(row[3:]),board_size=board_size) # 2차원 형태로 state 변경
+                rotate_12dir_states = get_rotate_board_12dir(board_2nd) # 12번 뒤집은 state
+                rotate_12dir_labels = get_rotate_label(label,board_size)
+                for i in range(len(rotate_12dir_states)):
+                    data_x_p_black.append(list(rotate_12dir_states[i]))
+                    labels_p_black.append(int(rotate_12dir_labels[i]))
             elif type_train == 1: # 백 정책망 학습
                 # if float(row[1]) >= 0.5 and int(float(row[2]) == 2):  # 백이 이기거나 비기는 경우면서 백이 돌을 놓을 차례인 경우
                 # 일단 전부 다 학습
-                labels_p_white.append(int(float(row[0])))
-                data_x_p_white.append(row[3:])
+                label = int(float(row[0])) # 정답 라벨
+                board_2nd = convert_1nd_board_to_2nd(np.array(row[3:]),board_size=board_size) # 2차원 형태로 state 변경
+                rotate_12dir_states = get_rotate_board_12dir(board_2nd) # 12번 뒤집은 state
+                rotate_12dir_labels = get_rotate_label(label,board_size)
+                for i in range(len(rotate_12dir_states)):
+                    data_x_p_white.append(list(rotate_12dir_states[i]))
+                    labels_p_white.append(int(rotate_12dir_labels[i]))
+            # elif type_train == 1: # 백 정책망 학습
+            #     if float(row[1]) >= 0.5 and int(float(row[2]) == 2):  # 백이 이기거나 비기는 경우면서 백이 돌을 놓을 차례인 경우
+                # 일단 전부 다 학습
+                # labels_p_white.append(int(float(row[0])))
+                # data_x_p_white.append(row[3:])
             elif type_train == 2:
                 labels_v.append(float(row[1]))
-                data_x_v.append(row[3:])
+                board_2nd = convert_1nd_board_to_2nd(np.array(row[3:]),board_size=board_size) # 2차원 형태로 state 변경
+                rotate_12dir_states = get_rotate_board_12dir(board_2nd) # 12번 뒤집은 state
+                for i in range(len(rotate_12dir_states)):
+                    data_x_v.append(list(rotate_12dir_states[i]))
+                    labels_v.append(float(row[1]))
             if count_read % 4000 == 0:
                 print("현재까지 읽은 row 수 :",count_read)
 
-    if len(data_x_p_black) >=1:
+    if len(data_x_p_black) >= 1:
         data_x_p_black = np.array(data_x_p_black, dtype=np.float32)
     if len(data_x_p_white) >= 1:
         data_x_p_white = np.array(data_x_p_white, dtype=np.float32)
@@ -95,11 +112,56 @@ def convert_load_dataset(csv_file_name, is_one_hot_encoding,type_train):
 def reshape_to_15_15_1(data):
     return K.reshape(data,[-1,15,15,1])
 
+# ex) 0~224 형태의 보드판을 15*15 형태의 numpy로 변경
+def convert_1nd_board_to_2nd(arr_1nd,board_size):
+    return np.reshape(arr_1nd,(board_size,board_size))
+
+def convert_2nd_board_to_1nd(arr_2nd):
+    return np.ravel(arr_2nd)
+
+
+# 0도(원본),90도,180도,270도 회전 + 각각마다 상하,좌우 반전 >> 하나의 상태로 12가지 상태 추출
+def get_rotate_board_12dir(arr):
+    all_list = []
+    for i in range(4):
+        rotates = np.rot90(arr,i)
+        flip_horizontal = np.fliplr(rotates)
+        flip_vertical = np.flipud(rotates)
+        all_list.append(rotates)
+        all_list.append(flip_horizontal)
+        all_list.append(flip_vertical)
+    return all_list
+
+def convert_to_label_start0(x, y,board_size):
+    return board_size * y + x
+
+def get_rotate_label(label,board_size):
+    arr = convert_label_to_board(label,board_size=board_size)
+    rotate_list = get_rotate_board_12dir(arr) # len = 12
+    all_list = []
+    for i in range(len(rotate_list)):
+        label_arr = rotate_list[i]
+        y,x = np.where(label_arr == 1)
+        move = convert_to_label_start0(x[0],y[0],board_size) # ndarray 형태로 나오기 때문에 0번으로 접근
+        all_list.append(move)
+    return all_list
+
+
+# move : 15*15의 경우 0~224
+# 레이블을 board형태로 전환
+def convert_label_to_board(move,board_size):
+    y = move//board_size
+    x = move-(y*board_size)
+    tmp = np.zeros([board_size,board_size])
+    tmp[y][x] = 1
+    return tmp
+
 # root mean squared error (rmse) for regression (only for Keras tensors)
 def rmse(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
 
 def get_model(model_type):
+
     model = None
     if model_type == 0: # 정책망 + Conv2D 6개
         model = Sequential()
@@ -173,7 +235,6 @@ def get_not_sequential_model():
 
 # pv_type : 'seperate' >> policy, value 분리망
 def get_dataset(csv_name,is_one_hot_encoding,pv_type,type_train):
-    csv_name = path_google_drive_main+csv_name
     if pv_type == 'seperate':
         data_x_p_black,data_x_p_white,data_y_p_black,data_y_p_white,data_x_v,data_y_v= convert_load_dataset(csv_name, is_one_hot_encoding=is_one_hot_encoding,type_train=type_train)
     else:
@@ -230,7 +291,7 @@ def train_model(model,csv_name,is_one_hot_encoding,batch_size):
     model.summary()
 
     type_train = int(input("훈련할 대상 : 0(흑 정책망) / 1(백 정책망) / 2(가치망)"))
-    data_x_p_black,data_x_p_white,data_y_p_black,data_y_p_white,data_x_v,data_y_v= get_dataset(csv_name,is_one_hot_encoding=is_one_hot_encoding,pv_type='seperate',type_train=type_train)
+    data_x_p_black,data_x_p_white,data_y_p_black,data_y_p_white,data_x_v,data_y_v= get_dataset(path_google_drive_main+csv_name,is_one_hot_encoding=is_one_hot_encoding,pv_type='seperate',type_train=type_train)
 
     if type_train == 0:
         data_y_p_black = to_categorical(data_y_p_black)
@@ -302,7 +363,7 @@ def test_model(model,csv_file_name,one_hot_encoding):
 
 
 if __name__ == '__main__':
-    to_do = int(input("처음 부터 생성 : 0 / 이어서 학습 : 1 / 테스트 : 2"))
+    to_do = int(input("처음 부터 생성 : 0 / 이어서 학습 : 1 / 테스트 데이터 : 2 / 기타 : 3"))
     one_hot_encoding = False
     batch_size = None
     if to_do == 0:
@@ -313,6 +374,10 @@ if __name__ == '__main__':
     elif to_do == 2:
       model_file_name = input(f"테스트에 사용할 모델 파일 (기본 경로 : {path_saved_model}")
       model = load_saved_model(model_file_name)
+    elif to_do == 3:
+        csv_name = input("csv name : ")
+        data_x_p_black,data_x_p_white,data_y_p_black,data_y_p_white,data_x_v,data_y_v= get_dataset(csv_name,is_one_hot_encoding=False,pv_type='seperate',type_train=0)
+        quit()
     else:
       print("없는 경우")
       quit()
